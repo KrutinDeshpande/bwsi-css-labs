d68fc1b93633f2c0bea90b802917f73749f23c25
KrutinDeshpande <krutin.pro68@gmail.com>
2026-03-13 15:50:15 -0500

    my intro on lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "krutin deshpande" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("I am a freshman")
 if __name__ == "__main__":
     main()
